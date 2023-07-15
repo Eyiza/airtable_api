4536082f7cd2dff191dae20395a1ba4fe991aff6
Eyiza <Precious.michael2002@gmail.com>
2023-07-15 12:14:38 +0100

    added cors

--- app.js
+++ app.js
@@ -5,11 +5,13 @@ const headerCheckerMiddleware = require('./middleware');
 const swaggerUi = require('swagger-ui-express');
 const docs = require('./docs');
 const morgan = require("morgan");
+const cors = require("cors");
 
 const app = express();
 
 app.use(morgan("dev"));
 app.use(bodyParser.json()); // To parse JSON data
+app.use(cors());
 app.use(headerCheckerMiddleware); // Use the middleware
 app.use('/api/airtable', router); // Use the router
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(docs));
